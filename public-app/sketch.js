4a72a1a6fbac86e957205362b0c1ce5727b41f76
Your Name <you@example.com>
2022-08-30 17:40:17 -0500

    Avances en app

--- public-app/sketch.js
+++ public-app/sketch.js
@@ -2,112 +2,35 @@ const NGROK = `https://${window.location.hostname}`;
 console.log('Server IP: ', NGROK);
 let socket = io(NGROK, { path: '/real-time' });
 
-let controllerX, controllerY = 0;
-let posX,posY = 0
-let interactions = 0;
-let message = "AguacateS"
-let isTouched = false;
- let c;
-
-
 function setup() {
-    frameRate(60);
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.style('z-index', '-1');
-    canvas.style('position', 'fixed');
-    canvas.style('top', '0');
-    canvas.style('right', '0');
-    controllerX = windowWidth / 2;
-    controllerY = windowHeight / 2;
-    posX = windowWidth/2;
-    posY = windowHeight/2;
-    background(0);
-    angleMode(DEGREES);
-   
-
-    socket.emit('device-size', {windowWidth, windowHeight});
-
-    let btn = createButton("Permitir movimiento");
-	btn.mousePressed(function(){
-		DeviceOrientationEvent.requestPermission(); //Necesario para manipular el device.
-	});
-
-}
-
-function draw() {
-    background(0, 5);
-    newCursor(pmouseX, pmouseY);
-    fill(255);
-    ellipse(controllerX, controllerY, 50, 50);
-    
-    c = color(255)
-    if(isTouched === true){
-        c = color(255,0,0)
+    // crear un lienzo
+    createCanvas(710, 400);
+  
+    input = createInput();
+    input.position(20, 65);
+  
+    button = createButton('submit');
+    button.position(150, 65);
+    button.mousePressed(greet);
+  
+    greeting = createElement('h2', 'what is your name?');
+    greeting.position(20, 5);
+  
+    textAlign(CENTER);
+    textSize(50);
+  }
+  
+  function greet() {
+    const name = input.value();
+    greeting.html('hello ' + name + '!');
+    input.value('');
+    for (let i = 0; i < 200; i++) {
+        push();
+        fill(random(255), 255, 255);
+        translate(random(width), random(height));
+        rotate(random(2 * PI));
+        text(name, 0, 0);
+        pop();
+      }
     }
-    fill(c);
-    square(30, 330, 55, 20)
-    square(30, 400, 55, 20)
-    square(100, 330, 55, 20)
-    square(100, 400, 55, 20)
-    square(170, 330, 55, 20)
-    square(170, 400, 55, 20)
-    square(240, 330, 55, 20)
-    square(240, 400, 55, 20)
-    ////////Tercera fila//////////
-    square(30, 470, 55, 20)
-    square(100, 470, 55, 20)
-    square(170, 470, 55, 20)
-    square(240, 470, 55, 20)
-    
-}
-
-/*function mouseDragged() {
-    socket.emit('positions', { controlX: pmouseX, controlY: pmouseY });
-}*/
-
-function touchMoved() {
-    switch (interactions) {
-        case 0:
-            socket.emit('mobile-instructions', { interactions, pmouseX, pmouseY,message });
-            background(255, 0, 0);
-            break;
-    }
-}
-
-function touchStarted(){
-    c = color(255);
-    isTouched = true;
-}
-
-function touchEnded(){
-    isTouched = false;
-}
-
-function deviceMoved() {
-    switch (interactions) {
-        case 1:
-            socket.emit('mobile-instructions', { interactions, pAccelerationX, pAccelerationY, pAccelerationZ });
-            background(0, 255, 255);
-            break;
-        case 2:
-            socket.emit('mobile-instructions', { interactions, rotationX, rotationY, rotationZ });
-            background(0, 255, 0);
-            break;
-    }
-    
-}
-
-function deviceShaken() {
-    //socket.emit('mobile-instructions', 'Moved!');
-    //background(0, 255, 255);
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-function newCursor(x, y) {
-    noStroke();
-    fill(255);
-    ellipse(x, y, 10, 10);
-}
+      
